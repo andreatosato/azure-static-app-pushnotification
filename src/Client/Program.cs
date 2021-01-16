0ed30851739c70c47808571aa750d1baca44640d
Andrea Tosato <andrea.tosato@hotmail.it>
2021-01-16 17:30:29 +0100

    Github add

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -28,7 +28,7 @@ namespace Blazoring.PWA.Client
             /*.AddFontAwesomeIcons()*/;
 
 #if DEBUG
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7071/api/") });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7071/api") });
 #else
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri($"{builder.HostEnvironment.BaseAddress}/api") });
 #endif
