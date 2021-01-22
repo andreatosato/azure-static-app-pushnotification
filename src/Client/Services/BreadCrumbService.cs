c541dcbc70d874b2130a5d253c167f7142ee8426
Andrea Tosato <andrea.tosato@hotmail.it>
2021-01-22 17:29:09 +0100

    BreadCrumb

---
+++ src/Client/Services/BreadCrumbService.cs
@@ -0,0 +1,37 @@
+﻿namespace Blazoring.PWA.Client.Services
+{
+	public class Routes
+	{
+		public static string Home() => "/";
+
+		public static string UsersList() => $"/{BreadCrum.UsersList}";
+		public static string UserEdit(int id) => $"/{BreadCrum.UserEdit}/{id}";
+		public static string UserCreate() => $"/{BreadCrum.UserCreate}";
+
+		public static string UsersGrid() => $"/{BreadCrum.UsersGrid}";
+		public static string Notifications() => $"/{BreadCrum.Notifications}";
+	}
+
+	public class BreadCrum
+	{
+		public const string Home = "";
+		public const string UsersList = "users";
+		public const string UserEdit = "users/edit";
+		public const string UserCreate = "users/create";
+		public const string UsersGrid = "usersgrid";
+		public const string Notifications = "notifications";
+	}
+
+	public class BreadCrumLink
+	{
+		public BreadCrumLink(string link, string description, bool active = false)
+		{
+			Link = link;
+			Description = description;
+			Active = active;
+		}
+		public string Link { get; }
+		public string Description { get; }
+		public bool Active { get; }
+	}
+}
